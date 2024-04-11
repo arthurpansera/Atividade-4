30894a44e0db97ae24c6db5e40f28aeb9e2a47e4
arthurpansera <arthurpansera0308@gmail.com>
2024-04-11 20:56:33 -0300

    Atividade 4
    Atividade 4: Exercícios (Repetição)

--- exercicio5.py
+++ exercicio5.py
@@ -6,11 +6,13 @@ pares = []
 impares = []
 
 for num in range(0,10):
-    num = int(input("Digite um número: "))
+    num = int(input(f"Digite o {num+1}º número: "))
     if num % 2 != 0:
         impares.append(num)
     else:
         pares.append(num)
 
+print(50*"-")
 print(f"Os números pares são: {pares}")
-print(f"Os números ímpares são; {impares}")
+print(f"Os números ímpares são: {impares}")
+print(50*"-")
